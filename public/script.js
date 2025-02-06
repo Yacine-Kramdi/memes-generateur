6fd9895b5a9654b666da476b69fa7a69ff413a34
Yacine-Kramdi <kramdiyacine@gmail.com>
2025-02-06 21:09:58 +0100

    ajouter de style

--- public/script.js
+++ public/script.js
@@ -4,8 +4,10 @@ function generateMeme (nom) {
     let ctx = canvas.getContext('2d');
     const text = document.getElementById("text-afficher").value;
     console.log(text);
+    ctx.font = '48px Impact';
+    ctx.fillStyle = 'white';
     ctx.textAlign = 'center';  
-    ctx.fillText(text, 150, 100);
+    ctx.fillText(text, 190, 150);
 }
 
 // pour afficher l'image dans le canva lors de note chargment de l'image instantatement
@@ -13,6 +15,7 @@ var input = document.getElementById('image');
 input.addEventListener('change', handleFiles);
 
 function handleFiles(e) {
+    document.getElementById("button-download").disabled = false;
     var ctx = document.getElementById('canvas').getContext('2d');
     var img = new Image;
     img.src = URL.createObjectURL(e.target.files[0]);
@@ -26,7 +29,7 @@ function handleFiles(e) {
 function downloadCanvas(){
     var canvas = document.querySelector("#canvas").toDataURL("image/png");
     var a = document.createElement('a');
-    a.download = "canvas.png";
+    a.download = "meme.png";
     a.href = canvas;
     a.click();
 }
